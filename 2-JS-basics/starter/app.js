c11166b254b844c2302e771e7bc488f866b2246d
Chijioke Nwagwu <wyhzest@gmail.com>
2019-03-23 12:29:20 +0000

    first commit

---
+++ 2-JS-basics/starter/app.js
@@ -0,0 +1,15 @@
+/*
+let firstName = 'wisest';
+let lastName = 'Chijioke';
+let age = '32';
+let nameAge = firstName + " " + age;
+alert(nameAge);
+*/
+
+// Math operators
+
+let birthYear,currentYear,age; 
+currentYear =parseInt(prompt('what year are we in?'));
+age = parseInt(prompt('how old are you?'));
+birthYear = currentYear - age;
+alert(/*'your year of birth is ' +*/ birthYear);
