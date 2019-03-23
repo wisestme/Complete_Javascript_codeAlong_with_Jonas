93899cff5a9caba1ec6f50c947706cc6c9651cea
Chijioke Nwagwu <wyhzest@gmail.com>
2019-03-23 13:43:44 +0000

    compares BMI of two individuals

--- 2-JS-basics/starter/app.js
+++ 2-JS-basics/starter/app.js
@@ -7,9 +7,24 @@ alert(nameAge);
 */
 
 // Math operators
-
+/*
 let birthYear,currentYear,age; 
 currentYear =parseInt(prompt('what year are we in?'));
 age = parseInt(prompt('how old are you?'));
 birthYear = currentYear - age;
-alert(/*'your year of birth is ' +*/ birthYear);
+alert('your year of birth is ' + birthYear);
+*/
+
+let heightGloria, heightChijioke, weightGloria, weightChijioke,
+bmiGloria, bmiChijioke, isBMIGloriaGreater;
+
+heightGloria = 1.76;
+heightChijioke = 1.86;
+weightChijioke = 55;
+weightGloria = 70;
+bmiGloria = weightGloria/(heightGloria * heightGloria);
+bmiChijioke = weightChijioke / (heightChijioke * heightChijioke);
+
+isBMIGloriaGreater = bmiGloria > bmiChijioke;
+
+alert("Is Gloria's BMI greater than Chijioke's? " + isBMIGloriaGreater);
